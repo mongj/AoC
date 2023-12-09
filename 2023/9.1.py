6423947e3114ed0c5a16c2612121b94b5019224c
Ming Jun <evan.zhangmingjun@gmail.com>
2023-12-09 22:20:35 +0800

    day 9

---
+++ 2023/9.1.py
@@ -0,0 +1,17 @@
+with open("9.sample.txt", "r") as f:
+    lines = f.readlines()
+
+def oasis_predict(seq):
+    if all(v == 0 for v in seq):
+        return 0
+    else:
+        next_line = [seq[i] - seq[i - 1] for i in range(1, len(seq))]
+        return seq[-1] + oasis_predict(next_line)
+
+pred = []
+
+for line in lines:
+    line = list(map(int, line.split()))
+    pred.append(oasis_predict(line))
+
+print(sum(pred))
